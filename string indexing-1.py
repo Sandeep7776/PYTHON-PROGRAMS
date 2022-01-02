f2ab76ed9aa786a43c2777a09285ddb9f552870a
Sandeep7776 <96559709+Sandeep7776@users.noreply.github.com>
2022-01-02 09:34:11 +0530

    Add files via upload

---
+++ string indexing-1.py
@@ -0,0 +1,13 @@
+course="information technology"
+
+#identify chars for index numbers: 10, -10, 12, 3, -5 ,5 0 ,-1 20
+
+print(course[10])
+print(course[-10])
+print(course[12])
+print(course[3])
+print(course[-5])
+print(course[5])
+print(course[0])
+print(course[-1])
+print(course[20])
