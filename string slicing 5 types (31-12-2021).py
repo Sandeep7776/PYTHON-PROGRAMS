8d3eca81a2505256d599df962ec9c781023d2a6e
Sandeep7776 <96559709+Sandeep7776@users.noreply.github.com>
2022-01-14 11:27:15 +0530

    Add files via upload

---
+++ string slicing 5 types (31-12-2021).py
@@ -0,0 +1,131 @@
+'''
+a= "Information technology_world"
+ #0 1 2 3 4 5 6 7 8 9 101 2 3 4 5 6 7 8 9 20 1 2 3 4 5
+ 
+print(a[1:5])# n f o r 
+print(a[2:-1]) #f o r m a t i o n_ t e c h n o l o g y_ w o r l
+print(a[-5:5]) #error
+print(a[-4:-1])# o r l
+print(a[:5]) #i n f o r
+print(a[2:]) #f o r m a t i o n_ t e c h n o l o g y_ w o r l d
+print(a[2:100])# f o r m a t i o n_ t e c h n o l o g y_ w o r l d
+print(a[:]) #i n f o r m a t i o n_ t e c h n o l o g y_ w o r l d
+
+print(a[-1:-5]) #error
+print(a[-2:0]) #error
+#print(a[5:1] #error
+
+print(a[2:-2:1])# f o r m a t i o n_ t e c h n o l o g y_ w o r
+print(a[2:-2:2]) #f r a i n t c n l g _ o
+print(a[2:-2:3]) #f m i _ c o g w l
+print(a[3:10:1]) #o r m a t i o 
+print(a[3:10:2]) #o m t o
+print(a[3:10:5]) #o i
+
+print(a[3: :3]) # o a o t h l y o d
+print(a[-5: : 2])# w r d
+print(a[:4:4]) #i 
+print(a[::3]) #i o a o t h l y o d
+
+
+#------------------------------------------------------------------------------------
+#01-01-2022
+
+a="Information_Technology"
+
+print(a[-2:0:-1]) # g o l o n h c e t _ n o i t a m r o f n
+print(a[5:-5:5])  #m n h 
+print(a[-5:5:-5]) #o t t
+print(a[5:-5:-5]) # error
+print(a[3:9:-1])  #error
+print(a[9:-10:-2]) #error
+print(a[2:-4:-4]) #error
+print(a[-4:2:-2]) #l n c t n i a r f
+print(a[0:-1:-3]) #error
+print(a[-3:3:6])  # error
+print(a[::-3])  # y l h t o a o i
+print(a[::-2])  # y o o h e _ o t m o n
+print(a[2:-2:-2]) #error
+print(a[2:-2:2]) # f r a i n t c n l
+print(a[5:-5:5]) # m n h 
+print(a[3:-3:3]) # o a o t h l  
+print(a[::2])    # f n a i n t c n l y
+print(a[::-5]) # y n _a n
+
+
+a="python_is_using_interpreter"
+
+print(a[-2:0:-1]) #e t e r p r e t n i _ g n i s u _ s i _ n o h t y
+print(a[5:-5:5]) # n u _ r
+print(a[-5:5:-5]) #r n i i 
+print(a[5:-5:-5]) # error
+print(a[3:9:-1]) #error
+print(a[9:-10:-2]) #error
+print(a[2:-4:-4]) #error
+print(a[-4:2:-2]) #e p e n _ n s _ i n h 
+print(a[0:-1:-3]) #errror
+print(a[-3:3:6]) #error
+print(a[::-3]) # r e r n g s s n t
+print(a[::-2]) # r t r r t i g i u s _ o t p
+print(a[2:-2:-2])# error
+print(a[2:-2:2]) #t o _ s u i g i t rr t r
+print(a[5:-5:5])# n u _ r 
+print(a[3:-3:3]) # h _ _i _ t P
+print(a[::2])# p t o _ s u i g _ n e p  e e
+print(a[::-5]) # p n u _r e
+
+
+
+a="computer science"
+
+print(a[-10:10:3]) # e s 
+print(a[-5:-1:1])  #i e n c
+print(a[-8:-2:10]) # _ 
+print(a[5::3])     #t _ i c 
+print(a[:5:2])     # c m u 
+print(a[10:14:2])  #c e
+print(a[-10:-3:3]) #e s e 
+print(a[5:12:5])   # t c
+print(a[3:-3:3])   #p e s e
+print(a[-5:-3:20]) #i
+
+
+a="Information_technology"
+
+
+print(a[-10:10:3]) # error
+print(a[-5:-1:1])  # o l o g
+print(a[-8:-2:10]) # c
+print(a[5::3])     # m i _ c o g
+print(a[:5:2])     # i f r 
+print(a[10:14:2])  #n t 
+print(a[-10:-3:3]) # t h l
+print(a[5:12:5])   # m n
+print(a[3:-3:3])   # o a o t h l
+print(a[-5:-3:20]) #o
+
+
+a="Google_chrome_browser"
+
+
+print(a[-10:10:3]) # error
+print(a[-5:-1:1])  # o w s e
+print(a[-8:-2:10]) # _
+print(a[5::3])     #e h m b w r
+print(a[:5:2])     # g o l
+print(a[10:14:2])  # o e
+print(a[-10:-3:3]) # m b w
+print(a[5:12:5])   # e o
+print(a[3:-3:3])   # g _ r e r 
+print(a[-5:-3:20]) # o
+'''
+
+
+
+
+
+
+
+
+
+
